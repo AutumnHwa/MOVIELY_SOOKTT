683522de6665f9b4c1b17a6eff820ffe93e049c5
autumn <autumn@autumnui-MacBookPro.local>
2024-07-29 19:32:08 +0900

    Fixed issue with rendering component

--- src/pages/MyalrPage.js
+++ src/pages/MyalrPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
